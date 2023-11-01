afe1e53c9cee7dd33a66fa78a4a76d87665e3eb9
Khalid Graine <khalidgraine1@gmail.com>
2023-11-01 12:29:54 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+      <nav class="bg-gray-100 flex gap-4">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+
+
+  <RouterView />
+</template>
